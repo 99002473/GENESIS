667f51c3d12c236417640f67c2a5c52462ee8d33
stepin104354 <69150993+stepin104354@users.noreply.github.com>
2020-09-19 00:40:45 +0530

    Main file definition

--- CalcConv/3_Implementation/calcconv.c
+++ CalcConv/3_Implementation/calcconv.c
@@ -31,4 +31,44 @@ int secval(int);
 int cosecval(int);
 int cotval(int);
 
-
+int main()
+{
+    int a,b;
+    int op;
+    printf("Enter the numerands\n");
+    scanf("%d %d",&a,&b);
+    printf("Enter the option for calc\n");
+    printf("Enter an operator (1, 2, 3, 4): ");
+    scanf("%d", &op);
+    int res;
+    switch (op)
+    {
+      case 1:
+        {      
+                res = add(a,b);
+                printf("%d", res);
+                break;
+        }
+        case 2:
+        {
+                res = sub(a,b);
+              printf("%d", res);
+              break;
+        }
+        case 3:
+        {
+                      res = mul(a,b);
+              printf("%d", res);
+              break;
+        }
+        case 4:
+        { 
+              res = div(a,b);
+              printf("%d", res);
+              break;
+        }
+        default:
+            printf("Error! operator is not correct");
+    }
+    return 0;
+}
