4899c5fe21c3143621e4ccbd28cd1f032280c2d3
99002473 <71481178+99002473@users.noreply.github.com>
2020-09-19 09:06:40 +0530

    Update calcconv.c

--- CalcConv/3_Implementation/calcconv.c
+++ CalcConv/3_Implementation/calcconv.c
@@ -31,113 +31,102 @@ int secval(int);
 int cosecval(int);
 int cotval(int);
 
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
-
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
-
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
-
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, AVERAGE, POWER, ROOT, PERCENT, FACTORIAL, COS, SIN, TAN, COSEC, SEC, COT, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
-
-
-/* Start of the application */
-int main(int argc, char *argv[])
+int main()
 {
-    printf("\n****Calculator****\n");
-    while(1)
+    int ch,op1,op2,res;
+    printf("Enter the operation you want to perform\n");
+    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Average\n6. Power\n7. Root\n8. Percent\n9. Root\n10. Percent\n11. Factoial\n12. Cos\n13. Sin\n14. Tan\n15. Cosec\n16. Sec\n17. Cot\n18. Exit\n");
+    switch (ch)
     {
-        calculator_menu();
-    }
-}
-
-void calculator_menu(void)
-{
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Average\n6. Power\n7. Root\n8. Percent\n9. Root\n10. Percent\n11. Factoial\n12. Cos\n13. Sin\n14. Tan\n15. Cosec\n16. Sec\n17. Cot\n18. Exit");
-    printf("\n\tEnter your choice\n");
-   
-     __fpurge(stdin);
-    scanf("%d", &calculator_operation);
-
-    if(EXIT == calculator_operation)
-    {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
-    }
-
-    if(INVALID != valid_operation(calculator_operation))
-    {
-        printf("\n\tEnter your Numbers with space between them\n");
-        __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
-    }
-    else
-    {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        __fpurge(stdin);
-        getchar();
-        return;
-        
-    }
-    switch(calculator_operation)
-    {
-        case ADD:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
+        case 1:
+           printf("enter the numbers\n");
+           scanf("%d %d",&op1,&op2);
+           res=add(op1,op2);
+           break;
+        case 2:
+           printf("enter the numbers\n");
+           scanf("%d %d",&op1,&op2);
+           res=sub(op1,op2);
+           break;
+        case 3:
+            printf("enter the numbers\n");
+            scanf("%d %d",&op1,&op2);
+            res=mul(op1,op2);
             break;
-        case SUBTRACT:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
+        case 4:
+             printf("enter the numbers\n");
+             scanf("%d %d",&op1,&op2);
+             res=div(op1,op2);
+             break;   
         case 5:
-            exit(0);
-            break;
+             printf("enter the numbers\n");
+             scanf("%d %d",&op1,&op2);
+             res=avg(op1,op2);
+             break;
+        case 6:
+             printf("enter the numbers\n");
+             scanf("%d %d",&op1,&op2);
+             res=power(op1,op2);
+             break;   
+        case 7:
+             printf("enter the number\n");
+             scanf("%d",&op1);
+             res=per(op1);
+             break;   
+        case 8:
+             printf("enter the number\n");
+             scanf("%d",&op1);
+             res=root(op1);
+             break;   
+        case 9:
+            printf("enter the numbers\n");
+             scanf("%d %d",&op1,&op2);
+             res=avg(op1,op2);
+             break;   
+        case 10:
+            printf("enter the number\n");
+             scanf("%d",&op1);
+             res=factorial(op1);
+             break;    
+        case 11:
+            printf("enter the number\n");
+             scanf("%d",&op1);
+             res=root(op1);
+             break;   
+        case 12:
+            printf("enter the number\n");
+             scanf("%d",&op1);
+             res=cosval(op1);
+             break;   
+        case 13:
+            printf("enter the number\n");
+             scanf("%d",&op1);
+             res=sinval(op1);
+             break;   
+        case 14:
+            printf("enter the number\n");
+             scanf("%d",&op1);
+             res=tanval(op1);
+             break;    
+        case 15:
+            printf("enter the number\n");
+             scanf("%d",&op1);
+             res=cosecval(op1);
+             break;   
+        case 16:     
+             printf("enter the number\n");
+             scanf("%d",&op1);
+             res=secval(op1);
+             break;
+        case 17:
+            printf("enter the number\n");
+             scanf("%d",&op1);
+             res=cotval(op1);
+             break;
+        case 18:
+            break;   
         default:
-            printf("\n\t---It should never come here---\n");
+            break;
     }
-}
 
-int valid_operation(int operation)
-{
-    /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
 }
